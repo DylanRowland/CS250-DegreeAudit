9a2b9790cff69460fceff82365a61286fd0c84b0
Javab05 <jboyd2321@sdsu.edu>
2026-03-18 15:19:03 -0700

    CourseEquivalency: check for bad data, transferability, and correlates units and classes between schools

--- backend/app/schema/Schema.py
+++ backend/app/schema/Schema.py
@@ -4,3 +4,9 @@ from typing import Optional, List
 class CourseInput(BaseModel):
     course_name: str
     community_college: str
+
+class CourseEquivalency (BaseModel):
+    cc_course_name: str
+    sdsu_course_name: str
+    units: int
+    is_transferable: bool
