5d66ad0f24ac16d54b678774cc1d3f7658d21d8a
nakanararas <nr.nakanararas@gmail.com>
2026-03-21 19:40:38 -0700

    Centering the form

--- app/form/form.tsx
+++ app/form/form.tsx
@@ -2,60 +2,63 @@
 export default function Form() {
     return (
         <div className="pb-20">
-            {/* Header */}
-            {/* Entering school */}
-            <p className="text-2xl" style={{ padding: "1rem"}}> Enter Your School: </p>
+            <div className="flex flex-col items-center">
+                {/* Entering school */}
+                <p className="text-2xl" style={{ padding: "1rem"}}> Enter Your School: </p>
 
-            <main style={{ padding: "1rem"}}>
-                <input className="border-2 border-black rounded"
-                    name="school"
-                    list = "schools"
-                    placeholder= "School name"
-                />
+                <main style={{ padding: "1rem"}}>
+                    <input className="border-2 border-black rounded"
+                        name="school"
+                        list = "schools"
+                        placeholder= "School name"
+                    />
 
-                <datalist id="schools">
-                    <option value="Grossmont College"></option>
-                    <option value="Cuyamaca College"></option>
+                    <datalist id="schools">
+                        <option value="Grossmont College"></option>
+                        <option value="Cuyamaca College"></option>
 
-                </datalist>
-            </main>
+                    </datalist>
+                </main>
 
-            {/* Entering major */}
-            <p className="text-2xl" style={{ padding: "1rem"}}> Enter Your Major:</p>
+                {/* Entering major */}
+                <p className="text-2xl" style={{ padding: "1rem"}}> Enter Your Major:</p>
 
-            <main style={{ padding: "1rem"}}>
-                <input className="border-2 border-black rounded"
-                    name="major"
-                    list = "majors"
-                    placeholder= "Major"
-                />
+                <main style={{ padding: "1rem"}}>
+                    <input className="border-2 border-black rounded"
+                        name="major"
+                        list = "majors"
+                        placeholder= "Major"
+                    />
 
-                <datalist id="majors">
-                    <option value="Computer Science"></option>
-                    <option value="Biology"></option>
-                    <option value="Business Administration"></option>
-                    <option value="English - Single Subject Teaching"></option>
-                    <option value="Economics"></option>
-                    <option value="History"></option>
-                    <option value="Liberal Studies Elementary Education"></option>
-                    <option value="Mathematics"></option>
-                    <option value="Political Science"></option>
-                    <option value="Psychology"></option>
-                </datalist>
-            </main>
+                    <datalist id="majors">
+                        <option value="Computer Science"></option>
+                        <option value="Biology"></option>
+                        <option value="Business Administration"></option>
+                        <option value="English - Single Subject Teaching"></option>
+                        <option value="Economics"></option>
+                        <option value="History"></option>
+                        <option value="Liberal Studies Elementary Education"></option>
+                        <option value="Mathematics"></option>
+                        <option value="Political Science"></option>
+                        <option value="Psychology"></option>
+                    </datalist>
+                </main>
 
-            {/* Entering courses */}
-            <p className="text-2xl" style={{ padding: "1rem"}}> Enter Courses You Have Taken:</p>
-            <main style={{ padding: "1rem"}}>
-                <textarea className="border-2 border-black rounded" name="message" rows="5" cols="40" placeholder= "Ex. Math 150">
-                </textarea>
-                
-                <p className="text-2xl" style={{ padding: "1rem", textIndent: "130px"}}> OR </p>
+                {/* Entering courses */}
+                <p className="text-2xl" style={{ padding: "1rem"}}> Enter Courses You Have Taken:</p>
+                <main style={{ padding: "1rem"}}>
+                    <div className = "flex justify-center p-4">
+                        <textarea className="border-2 border-black rounded" name="message" rows="5" cols="40" placeholder= "Ex. Math 150">
+                        </textarea>
+                    </div>
+                    
+                    <p className="text-2xl text-center p-4" style={{ padding: "1rem"}}> OR </p>
 
-                <label htmlFor="myfile">File must be .JSON type: </label>
-                    <input className="border-2 border-black rounded" type="file" id="myfile" name="myfile"/>
-                        <br /><br />
-            </main>
+                    <label htmlFor="myfile">File must be .JSON type: </label>
+                        <input className="border-2 border-black rounded" type="file" id="myfile" name="myfile"/>
+                            <br /><br />
+                </main>
+            </div>
 
             {/* Submit Button */}
             <div className="flex justify-center mt-12">
