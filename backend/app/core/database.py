dd8be8fe05b5498e91847825c373b1d78a4804a7
Javab05 <jboyd2321@sdsu.edu>
2026-03-23 13:27:34 -0700

    put_json_in_bucket gets two things(filename and data) and does the opposite (dict->string->R2)

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -16,3 +16,10 @@ def get_json_from_bucket(filename: str):
     response_body = json.loads(response["Body"].read().decode("utf-8"))
 
     return response_body
+
+def put_json_in_bucket(filename: str, data: dict):
+    client = get_R2_client()
+
+    json_body = json.dumps(data)
+
+    client.put_object(Bucket = settings.R2_BUCKET_NAME, Key = filename, Body = json_body)
