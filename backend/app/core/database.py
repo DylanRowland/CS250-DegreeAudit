6077ec3da25241ae3a10acaf63727db27bf4d45a
Javab05 <jboyd2321@sdsu.edu>
2026-03-23 13:10:56 -0700

    get_json_from_bucket retrieves the data associated with filename from bucket, decodes it, and pushes it as usable data

---
+++ backend/app/core/database.py
@@ -0,0 +1,18 @@
+import boto3
+import json
+from backend.app.core.config import settings
+
+def get_R2_client():
+    s3 = boto3.client("s3", endpoint_url = settings.R2_ENDPOINT_URL,
+    aws_access_key_id = settings.R2_ACCESS_KEY,
+    aws_secret_access_key = settings.R2_SECRET_KEY)
+    return s3
+
+def get_json_from_bucket(filename: str):
+    client = get_R2_client()
+
+    response = client.get_object(Bucket = settings.R2_BUCKET_NAME, Key = filename)
+
+    response_body = json.loads(response["Body"].read().decode("utf-8"))
+
+    return response_body
