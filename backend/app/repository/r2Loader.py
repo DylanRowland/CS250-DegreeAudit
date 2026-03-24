bbded8da4d0dab417a1bde805d74cba09e6469d9
TreeTri123 <triannanguyentu@gmail.com>
2026-03-23 23:20:54 -0700

    Added import settings, because of errors

--- backend/app/repository/r2Loader.py
+++ backend/app/repository/r2Loader.py
@@ -1,39 +1,35 @@
 import boto3
 import json
+from backend.app.core.config import settings
 
 def get_r2_client():
     s3 = boto3.client(
-        service_name = 's3',
-        endpoint_url = R2_URL,
-        aws_access_key_id = ACCESS_KEY_ID,
-        aws_secret_access_key = SECRET_KEY_ID,
-        region_name = 'auto',  
+        service_name='s3',
+        endpoint_url=settings.R2_ENDPOINT_URL,
+        aws_access_key_id=settings.R2_ACCESS_KEY,
+        aws_secret_access_key=settings.R2_SECRET_KEY,
+        region_name='auto',  
     )
     return s3
 
 def load_json_file(filename):
     s3 = get_r2_client()
-    response = s3.get_object(Bucket = 'eagle-eyed-scholars', Key = filename)
+    response = s3.get_object(Bucket=settings.R2_BUCKET_NAME, Key=filename)
     file_content = response['Body'].read().decode('utf-8')
     json_content = json.loads(file_content)
     return json_content
 
 def load_courses():
-    courses_json = load_json_file('courses.json')
-    return courses_json
+    return load_json_file('courses.json')
 
 def load_course_equivalencies():
-    course_equivalencies_json = load_json_file('course_equivalencies.json')
-    return course_equivalencies_json
+    return load_json_file('course_equivalencies.json')
 
 def load_course_tags():
-    course_tags_json = load_json_file('course_tags.json')
-    return course_tags_json
+    return load_json_file('course_tags.json')
 
 def load_majors_programs():
-    majors_programs_json = load_json_file('majors_programs.json')
-    return majors_programs_json
+    return load_json_file('majors_programs.json')
 
 def load_schools():
-    schools_json = load_json_file('schools.json')
-    return schools_json
+    return load_json_file('schools.json')
