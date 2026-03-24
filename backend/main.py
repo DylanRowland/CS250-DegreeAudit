23aa17e5497ce089397f7c65ecb0ad7679ebbb6f
TreeTri123 <triannanguyentu@gmail.com>
2026-03-23 22:57:26 -0700

    Main.py added reference to routes

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from app.core.startup import configure_app
+from app.api.v1.routes import api_v1_router
 
 app = FastAPI(title="Eagle-Eyed Scholars")
 configure_app(app)
+
+app.include_router(api_v1_router)
 
 @app.get("/")
 def root():
