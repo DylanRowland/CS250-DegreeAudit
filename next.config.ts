847b0aa51f60ef7b879e1bb3fa9a2c7d9a331fff
LucasGuzylak <lucasguzy11@gmail.com>
2026-05-10 15:54:26 -0700

    changed next.config.ts to have backend proxy rewrites

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+// Local Python server location
+const BACKEND_ORIGIN = "http://127.0.0.1:8000";
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        // API call so frontend and backend can communicate
+        source: "/api/v1/:path*",
+        destination: `${BACKEND_ORIGIN}/api/v1/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
